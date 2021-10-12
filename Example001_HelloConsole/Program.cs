78d1b8e5d1b24043a850a098c129b4c5dd1a7775
Sergey Kamyanetskiy <i@ksergey.ru>
2021-10-12 09:33:49 +0300

    add Example001 HelloConsole

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Hello World!");
+
+//
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="args"></param>
+/// <returns></returns>
+
+#region Много Hello world'ов
+
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+System.Console.WriteLine("Hello World!");
+
+#endregion
+
+// TODO: Добавление нового функционала, лайкусик
+// HACK: Думать некогда, делать
+// UNDONE: Нужно что-то доделать, подписаться на канал
+// UnresolvedMergeConflict 
+// ERROR: У нас в команде так принято!
