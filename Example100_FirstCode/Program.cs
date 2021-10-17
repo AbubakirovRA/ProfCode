bb331c71062bb03a7b95a2a37867a1d39f09dec3
ksergey <ksergey>
2021-10-17 06:01:13 +0300

    update Example100_FirstCode

--- Example100_FirstCode/Program.cs
+++ Example100_FirstCode/Program.cs
@@ -10,30 +10,61 @@ using static System.Console;
 // Показать результат
 // Подумать над тем, какие проблемы созданы или могут быть созданы
 
-// Шаг первый: решить как-нибудь
+#region Шаг первый: решить как-нибудь
+// Write("Введите первое число: ");
+// string input = Console.ReadLine();
+// double x = double.Parse(input);
+// Write("Введите второе число: ");
+// input = Console.ReadLine();
+// double y = double.Parse(input);
+// Write("Введите третье число: ");
+// input = Console.ReadLine();
+// double z = double.Parse(input);
+// WriteLine($"x:{x}  y:{y}  z:{z}");
+// Write("Введите операцию: / * + - ^: ");
+// string operation = Console.ReadLine();
+// double result = 0;
+// if (operation == "/") result = x / y / z;
+// if (operation == "*") result = x * y * z;
+// if (operation == "+") result = x + y + z;
+// if (operation == "-") result = x - y - z;
+// if (operation == "^") result = Math.Pow(x,Math.Pow(y,z));
+// WriteLine($"result = {result}");
+#endregion
 
+#region Шаг второй: решить как-нибудь
 Write("Введите первое число: ");
 string input = Console.ReadLine();
-int x = int.Parse(input);
+double x = double.Parse(input);
 
 Write("Введите второе число: ");
 input = Console.ReadLine();
-int y = int.Parse(input);
+double y = double.Parse(input);
 
 Write("Введите третье число: ");
 input = Console.ReadLine();
-int z = int.Parse(input);
+double z = double.Parse(input);
 
 WriteLine($"x:{x}  y:{y}  z:{z}");
 
 Write("Введите операцию: / * + - ^: ");
 string operation = Console.ReadLine();
 
-int result = 0;
-if (operation == "/") result = x / y / z;
-if (operation == "*") result = x * y * z;
-if (operation == "+") result = x + y + z;
-if (operation == "-") result = x - y - z;
-if (operation == "^") result = x ^ y ^ z;
+//// double result = 0;
+//// if (operation == "/") result = x / y / z;
+//// if (operation == "*") result = x * y * z;
+//// if (operation == "+") result = x + y + z;
+//// if (operation == "-") result = x - y - z;
+//// if (operation == "^") result = Math.Pow(x,Math.Pow(y,z));
+
+double result = operation switch
+{
+    "/" => x / y / z,
+    "*" => x * y * z,
+    "-" => x - y - z,
+    "+" => x + y + z,
+    _ => Math.Pow(Math.Pow(x, y),z)
+};
 
 WriteLine($"result = {result}");
+#endregion
