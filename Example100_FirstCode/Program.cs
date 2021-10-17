ddc3bc43d49af752313d3d6dec63223c597fb356
Sergey Kamyanetskiy <i@ksergey.ru>
2021-10-17 05:04:52 +0300

    add Example100 FirstCode Simple Calculator

---
+++ Example100_FirstCode/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+// Необычный калькулятор
+
+// Получить первое число
+// Получить второе число
+// Получить третье число
+// Определите какие операции может выполнять ваш калькулятор
+// Спросить какую операцию выполнить
+// Показать результат
+// Подумать над тем, какие проблемы созданы или могут быть созданы
+
+// Шаг первый: решить как-нибудь
+
+Write("Введите первое число: ");
+string input = Console.ReadLine();
+int x = int.Parse(input);
+
+Write("Введите второе число: ");
+input = Console.ReadLine();
+int y = int.Parse(input);
+
+Write("Введите третье число: ");
+input = Console.ReadLine();
+int z = int.Parse(input);
+
+WriteLine($"x:{x}  y:{y}  z:{z}");
+
+Write("Введите операцию: / * + - ^: ");
+string operation = Console.ReadLine();
+
+int result = 0;
+if (operation == "/") result = x / y / z;
+if (operation == "*") result = x * y * z;
+if (operation == "+") result = x + y + z;
+if (operation == "-") result = x - y - z;
+if (operation == "^") result = x ^ y ^ z;
+
+WriteLine($"result = {result}");
